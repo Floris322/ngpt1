68cc70ca052bc5109b0870728f8a1dffa3c861cb
Floris Vegter <23455519+Floris322@users.noreply.github.com>
2020-11-16 12:58:30 +0100

    _

--- bundle.js
+++ bundle.js
@@ -7,13 +7,20 @@ class Game {
         this.viewport = document.createElement('canvas');
         this.context = this.viewport.getContext('2d');
 
-        this.viewport.width = 800;
-        this.viewport.height = 600;
+        this.viewport.width = this.constants.width;
+        this.viewport.height = this.constants.height;
 
+        //~~
         container.insertBefore(this.viewport, container.firstChild);
         this.context.fillStyle = '#FF00FF';
         this.context.fillRect(20, 20, 150, 100);
     }
+    constants = {
+        width: 800,
+        height: 600
+    };
+
+    state = {};
 }
 
 window.game = new Game();
