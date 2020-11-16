2cfec2631c0c5342b09fbaf685ad333ec6c86796
Floris Vegter <23455519+Floris322@users.noreply.github.com>
2020-11-16 13:00:01 +0100

    _

--- js/game.js
+++ js/game.js
@@ -9,8 +9,11 @@ class Game {
         this.viewport.width = this.constants.width;
         this.viewport.height = this.constants.height;
 
-        //~~
         container.insertBefore(this.viewport, container.firstChild);
+        
+        //~~
+        this.context.fillStyle = '#DDDDDD';
+        this.context.fillRect(0, 0, this.constants.width, this.constants.height);
         this.context.fillStyle = '#FF00FF';
         this.context.fillRect(20, 20, 150, 100);
     }
